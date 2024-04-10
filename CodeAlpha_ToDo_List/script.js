3b0f0cfdedc3316419ae6a884ac043ea1b8912fa
dipanshuuk711 <dipanshuuk711@gmail.com>
2024-04-10 19:37:46 +0530

    Fixed Bugs

--- CodeAlpha_ToDo_List/script.js
+++ CodeAlpha_ToDo_List/script.js
@@ -3,19 +3,25 @@ let ul = document.querySelector('ul');
 let input = document.querySelector('input');
 
 btn.addEventListener('click', () => {
-     let newTask = document.createElement('li');
-     newTask.innerText = input.value;
+     if (input.value == '') {
+          alert("Enter The Task")
+     }
+     else {
+          let newTask = document.createElement('li');
+          newTask.innerText = input.value;
 
-     let delBtn = document.createElement('button');
-     delBtn.innerText = 'Delete';
-     delBtn.classList.add("delete");
-     delBtn.classList.add("btn");
-     delBtn.classList.add("btn-primary");
+          let delBtn = document.createElement('button');
+          delBtn.innerText = 'Delete';
+          delBtn.classList.add("delete");
+          delBtn.classList.add("btn");
+          delBtn.classList.add("btn-primary");
+          newTask.classList.add("mt-2")
 
-     newTask.appendChild(delBtn);
-     delBtn.addEventListener('click', () => {
-          newTask.remove();
-     });
-     ul.appendChild(newTask);
-     input.value = ''
+          newTask.appendChild(delBtn);
+          delBtn.addEventListener('click', () => {
+               newTask.remove();
+          });
+          ul.appendChild(newTask);
+          input.value = ''
+     }
 });
